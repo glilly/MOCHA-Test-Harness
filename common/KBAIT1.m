b6484ccfdac8af7925f7952ff8146c7f696113e9
George Lilly <glilly@glilly.net>
2013-03-28 20:04:11 +0000

    added parsing code - getting errors from the MXML parser

--- common/KBAIT1.m
+++ common/KBAIT1.m
@@ -26,16 +26,39 @@ PARSE(ZNUM) ; parse ien ZNUM and put the DOM in xml storage
  N ZLAST S ZLAST=$O(@GN@(""),-1) ; end of xml array
  ;I @GN@(ZLAST)'["PEPS" S @GN@(ZLAST+1)="</PEPSResponse>"
  I @GN@(ZLAST)'["</drugInfoResponse" S @GN@(ZLAST+1)="</drugInfoResponse>"
- S KBAIDID=$$EN^MXMLDOM($NA(@GN@("XML")),"W")
+ S KBAIDID=$$EN^MXMLDOM($NA(@GN@("XML")),"V")
  I KBAIDID=0 D  Q  ;
  . ZWRITE ^TMP("MXMLERR",$J,*)
  M ^KBAI("MOCHA",ZNUM,"DOM")=^TMP("MXMLDOM",$J,KBAIDID)
+ Q
+ ;
+PARSE2(ZXML) ; parse ien ZNUM and put the DOM in xml storage
+ N KBAIDID
+ ;W !,"Parsing file ",^KBAI("MOCHA",ZNUM,"NAME"),!
+ ;N GN S GN=$NA(^KBAI("MOCHA",ZNUM,"XML"))
+ S GN=$NA(ZXML)
+ N ZLAST S ZLAST=$O(@GN@(""),-1) ; end of xml array
+ S KBAIDID=$$EN^MXMLDOM($NA(@GN@("XML")),"W")
+ I KBAIDID=0 D  Q  ;
+ . ZWRITE ^TMP("MXMLERR",$J,*)
+ ;M ^KBAI("MOCHA",ZNUM,"DOM")=^TMP("MXMLDOM",$J,KBAIDID)
  Q
  ;
 FIXXML ; fix xml errors due to reading the xml files into VistA
  N ZI S ZI=1
  N GN S GN=$NA(^KBAI("MOCHA",ZI,"XML"))
  S @GN@(3)=$TR(@GN@(3)," ","") ; remove blanks from attribute line
+ Q
+ ;
+FIXXML2 ; fix the missing final tags in the xml
+ N ZI S ZI=1
+ F  S ZI=$O(^KBAI("MOCHA",ZI)) Q:'ZI  D  ;
+ . N GN S GN=$NA(^KBAI("MOCHA",ZI,"XML"))
+ . N LAST S LAST=$O(@GN@(""),-1)
+ . N TOP S TOP=$P(@GN@(2),"<",2)
+ . N BOT S BOT=$P(@GN@(LAST),"</",2)
+ . I TOP'=BOT S @GN@(LAST+1)="</"_TOP
+ . I $E($L(TOP))'=">" S @GN@(LAST+1)=@GN@(LAST+1)_">"
  Q
  ;
 LOAD ; load mocha test xml files into ^KBAI("MOCHA") and index
@@ -67,6 +90,7 @@ LOAD ; load mocha test xml files into ^KBAI("MOCHA") and index
  . ;N GN1 S GN1=$NA(@GR@(ZL,"XML",1,0))
  . N GN1 S GN1=$NA(@GR@(ZL,"XML",1))
  . Q:$$FTG^%ZISH(DIRNAME,FNAME,GN1,4)=""
+ D FIXXML2 ; make sure that final tags are correct
  Q
  ;
 MOCHATXF ; mocha test xml files
